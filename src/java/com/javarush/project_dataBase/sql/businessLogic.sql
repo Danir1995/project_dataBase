455ac8ffe37880ac798e0250f577428241688356
DanirPT <danir.hucaev@gmail.com>
2023-01-22 10:51:10 +0000

    made update_block_note and del_block_note triggers

--- src/java/com/javarush/project_dataBase/sql/businessLogic.sql
+++ src/java/com/javarush/project_dataBase/sql/businessLogic.sql
@@ -18,6 +18,7 @@ CREATE TABLE changes_by_user(
     last_update timestamp default current_timestamp
 );
 
+
 CREATE OR REPLACE FUNCTION put_block_note() RETURNS TRIGGER
     AS $$
     BEGIN
@@ -30,4 +31,32 @@ DROP TRIGGER tr_put_block_note ON block_notes cascade;
 
 CREATE TRIGGER tr_put_block_note AFTER INSERT ON block_notes
 FOR EACH ROW WHEN (pg_trigger_depth() < 1) EXECUTE PROCEDURE put_block_note();
+
+
+CREATE OR REPLACE FUNCTION del_block_note() RETURNS TRIGGER
+AS $$
+BEGIN
+    insert into changes_by_user(user_id, what_changed, actions) values ((select user_id from users order by user_id desc limit 1) ,new.name, 'deleted') ;
+    return new;
+end;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER tr_del_block_note ON block_notes cascade;
+
+CREATE TRIGGER tr_del_block_note AFTER DELETE ON block_notes
+FOR EACH ROW WHEN (pg_trigger_depth() < 1) EXECUTE PROCEDURE del_block_note();
+
+
+CREATE OR REPLACE FUNCTION update_block_note() RETURNS TRIGGER
+AS $$
+BEGIN
+    insert into changes_by_user(user_id, what_changed, actions) values ((select user_id from users order by user_id desc limit 1) ,new.name, 'updated') ;
+    return new;
+end;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER tr_update_block_note ON block_notes cascade;
+
+CREATE TRIGGER tr_update_block_note AFTER UPDATE ON block_notes
+FOR EACH ROW WHEN (pg_trigger_depth() < 1) EXECUTE PROCEDURE update_block_note();
 
