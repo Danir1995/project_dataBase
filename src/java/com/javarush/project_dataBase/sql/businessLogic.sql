0693c33dc8855d330c9f3db9ba25dfc4af7332c8
DanirPT <danir.hucaev@gmail.com>
2023-01-22 01:08:50 +0000

    added businessLogic.sql and created table changes_by_user

---
+++ src/java/com/javarush/project_dataBase/sql/businessLogic.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE FUNCTION last_update_in_notes() RETURNS TRIGGER
+AS $$
+    BEGIN
+    UPDATE notes SET last_update = NOW() WHERE note_id = OLD.note_id;
+    RETURN OLD;
+    END
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS tr_last_update_in_notes on entries cascade ;
+CREATE TRIGGER tr_last_update_in_notes AFTER DELETE OR UPDATE OR INSERT ON entries
+FOR EACH ROW WHEN (pg_trigger_depth() < 1 ) EXECUTE PROCEDURE last_update_in_notes();
+
+
+CREATE TABLE changes_by_user(
+    user_id bigint references users(user_id) on delete cascade,
+    what_changed varchar,
+    actions varchar,
+    last_update timestamp default current_timestamp
+);
+
+CREATE OR REPLACE FUNCTION put_block_note() RETURNS TRIGGER
+    AS $$
+    BEGIN
+    insert into changes_by_user(user_id, what_changed, actions) values ((select user_id from users order by user_id desc limit 1) ,new.name, 'inserted') ;
+    return new;
+    end;
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER tr_put_block_note ON block_notes cascade;
+
+CREATE TRIGGER tr_put_block_note AFTER INSERT ON block_notes
+FOR EACH ROW WHEN (pg_trigger_depth() < 1) EXECUTE PROCEDURE put_block_note();
+
